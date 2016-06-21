257ebb529a965d6b0bd3b37f496abea91a53efad
phirunson <phirun@aligent.com.au>
2016-06-21 11:32:28 +0930

    Refactor beforetopsearch.phtml to use block methods and cleanup

--- Block/Algolia.php
+++ Block/Algolia.php
@@ -1,60 +1,90 @@
 <?php
-
 namespace Algolia\AlgoliaSearch\Block;
 
 use Algolia\AlgoliaSearch\Helper\ConfigHelper;
+use Algolia\AlgoliaSearch\Helper\Entity\ProductHelper;
 use Magento\Customer\Model\Session;
 use Magento\Framework\App\ObjectManager;
-use Magento\Framework\App\Request\Http;
 use Magento\Framework\Data\Form\FormKey;
 use Magento\Framework\Locale\Currency;
-use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\Registry;
 use Magento\Framework\View\Element\Template;
-use Magento\Search\Helper\Data;
-use Magento\Store\Model\StoreManagerInterface;
+use Magento\Search\Helper\Data as CatalogSearchHelper;
 
-class Algolia extends Template implements \Magento\Framework\Data\CollectionDataSourceInterface
+class Algolia extends Template
 {
-    public $config;
-    public $catalogSearchHelper;
-    public $customerSession;
-    public $storeManager;
-    public $request;
-    public $form;
-    public $objectManager;
-    public $registry;
-    public $productHelper;
+    protected $config;
+    protected $catalogSearchHelper;
+    protected $customerSession;
+    protected $storeManager;
+    protected $objectManager;
+    protected $registry;
+    protected $productHelper;
+    protected $currency;
 
-    public function __construct(ConfigHelper $config,
-                                Data $catalogSearchHelper,
-                                Template\Context $context,
-                                Session $customerSession,
-                                \Algolia\AlgoliaSearch\Helper\Entity\ProductHelper $productHelper,
-                                StoreManagerInterface $storeManager,
-                                Http $request,
-                                FormKey $form,
-                                Currency $currency,
-                                ObjectManagerInterface $objectManager,
-                                Registry $registry,
-                                array $data = [])
-    {
-        $this->config               = $config;
-        $this->catalogSearchHelper  = $catalogSearchHelper;
-        $this->customerSession      = $customerSession;
-        $this->storeManager         = $storeManager;
-        $this->request              = $request;
-        $this->form                 = $form;
-        $this->objectManager        = $objectManager;
-        $this->registry             = $registry;
-        $this->productHelper        = $productHelper;
-        $this->currency             = $currency;
+    protected $priceKey;
+
+    public function __construct(
+        Template\Context $context,
+        ConfigHelper $config,
+        CatalogSearchHelper $catalogSearchHelper,
+        Session $customerSession,
+        ProductHelper $productHelper,
+        Currency $currency,
+        Registry $registry,
+        array $data = []
+    ) {
+        $this->config = $config;
+        $this->catalogSearchHelper = $catalogSearchHelper;
+        $this->customerSession = $customerSession;
+        $this->productHelper = $productHelper;
+        $this->currency = $currency;
+        $this->registry = $registry;
 
         parent::__construct($context, $data);
     }
 
-    public function getAssetRepository()
+    public function getConfigHelper()
     {
-        return $this->_assetRepo;
+        return $this->config;
+    }
+
+    public function getProductHelper()
+    {
+        return $this->productHelper;
+    }
+
+    public function getCatalogSearchHelper()
+    {
+        return $this->catalogSearchHelper;
+    }
+
+    public function getCurrencySymbol()
+    {
+        return $this->currency->getCurrency($this->getCurrencyCode())->getSymbol();
+    }
+    public function getCurrencyCode()
+    {
+        return $this->_storeManager->getStore()->getCurrentCurrencyCode();
+    }
+
+    public function getPriceKey()
+    {
+        if ($this->priceKey === null) {
+            $groupId = $this->customerSession->getCustomer()->getGroupId();
+            $currencyCode = $this->getCurrencyCode();
+            $this->priceKey = $this->config->isCustomerGroupsEnabled($this->_storeManager->getStore()->getStoreId()) ? '.' . $currencyCode . '.group_' . $groupId : '.' . $currencyCode . '.default';
+        }
+        return $this->priceKey;
+    }
+
+    public function getStoreId()
+    {
+        return $this->storeManager->getStore()->getStoreId();
+    }
+    
+    public function getCurrentCategory()
+    {
+        return $this->registry->registry('current_category');
     }
 }

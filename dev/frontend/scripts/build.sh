7357ff30fa96cec9696e6ee350b9be24f4e87c4c
vvo <vincent.voyer@algolia.com>
2017-02-22 21:59:51 -0800

    fix(amd): fix algolia admin bundle
    fixes #158

--- dev/frontend/scripts/build.sh
+++ dev/frontend/scripts/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-set -e # exit when error
-set -x # debug messages
 
-npm install algoliasearch-extensions-bundle@latest --save
-cp node_modules/algoliasearch-extensions-bundle/dist/algoliaBundle.min.js* ../../view/frontend/web/
+yarn add algoliasearch-extensions-bundle@latest --save &&
+(cd node_modules/algoliasearch-extensions-bundle && npm run build) &&
+cp node_modules/algoliasearch-extensions-bundle/dist/algoliaBundle.min.js* ../../view/frontend/web/internals &&
 cp node_modules/algoliasearch-extensions-bundle/dist/algoliaAdminBundle.min.js* ../../view/adminhtml/web/

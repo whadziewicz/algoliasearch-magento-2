455e5db0b3b3b4e7e0f23a89994a0f267ccfbdc8
Jan Petr <honzik.petr@gmail.com>
2018-01-15 14:01:59 +0100

    Test search

---
+++ Test/Integration/SearchTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Test\Integration;
+
+use Algolia\AlgoliaSearch\Helper\Data;
+use Algolia\AlgoliaSearch\Model\Indexer\Product;
+use AlgoliaSearch\AlgoliaException;
+
+class SearchTest extends TestCase
+{
+    public function testSearch()
+    {
+        /** @var Product $indexer */
+        $indexer = $this->getObjectManager()->create('\Algolia\AlgoliaSearch\Model\Indexer\Product');
+        $indexer->executeFull();
+
+        $this->algoliaHelper->waitLastTask();
+
+        /** @var Data $helper */
+        $helper = $this->getObjectManager()->create('Algolia\AlgoliaSearch\Helper\Data');
+        $results = $helper->getSearchResult('', 1);
+
+        $this->assertNotEmpty($results);
+    }
+}

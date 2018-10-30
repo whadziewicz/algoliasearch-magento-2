25e276f0e4ff41a7999e66bc2f7e9e229477fd57
Damien Couchez <damien.couchez@gmail.com>
2018-10-30 11:49:45 +0100

    Remove color facet query rule (#599)
    * Remove the default color facet query rule

--- Test/Integration/ConfigTest.php
+++ Test/Integration/ConfigTest.php
@@ -50,7 +50,7 @@ class ConfigTest extends TestCase
             $page++;
         } while (($page * $hitsPerPage) < $fetchedQueryRules['nbHits']);
 
-        $this->assertEquals(1, count($matchedRules));
+        $this->assertEquals(0, count($matchedRules));
     }
 
     public function testAutomaticalSetOfCategoriesFacet()

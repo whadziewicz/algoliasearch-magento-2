36b5edffb5642c8dc051e17fb552eef781a03c00
Damien Couchez <damien.couchez@gmail.com>
2018-11-08 17:54:19 +0100

    Fix deactivated serialized arrays (#593)
    * Fix deactivated serialized arrays : when IS or autocomplete are turned off in the Magento configuration, prevent serialized array fields to be emptied.

---
+++ Controller/Adminhtml/System/Config/Save.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Controller\Adminhtml\System\Config;
+
+class Save extends \Magento\Config\Controller\Adminhtml\System\Config\Save
+{
+    private $instantSerializedValues = ['facets', 'sorts'];
+    private $autocompleteSerializedValues = ['sections', 'excluded_pages'];
+
+    protected function _getGroupsForSave()
+    {
+        $groups = parent::_getGroupsForSave();
+
+        return $this->handleDeactivatedSerializedArrays($groups);
+    }
+
+    private function handleDeactivatedSerializedArrays($groups)
+    {
+        if (isset($groups['instant']['fields']['is_instant_enabled']['value'])
+                && $groups['instant']['fields']['is_instant_enabled']['value'] == '0') {
+            foreach ($this->instantSerializedValues as $field) {
+                if (isset($groups['instant']['fields'][$field])) {
+                    unset($groups['instant']['fields'][$field]);
+                }
+            }
+        }
+
+        if (isset($groups['autocomplete']['fields']['is_popup_enabled']['value'])
+                && $groups['autocomplete']['fields']['is_popup_enabled']['value'] == '0') {
+            foreach ($this->autocompleteSerializedValues as $field) {
+                if (isset($groups['autocomplete']['fields'][$field])) {
+                    unset($groups['autocomplete']['fields'][$field]);
+                }
+            }
+        }
+
+        return $groups;
+    }
+}

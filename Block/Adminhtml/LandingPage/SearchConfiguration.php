cf67904717dacaa9ff503925231dc3e3224e5fe6
Damien Couchez <damien.couchez@gmail.com>
2018-12-31 16:12:29 +0100

    Add search confguration widget in the form

---
+++ Block/Adminhtml/LandingPage/SearchConfiguration.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Block\Adminhtml\LandingPage;
+
+use Algolia\AlgoliaSearch\Helper\ConfigHelper;
+use Algolia\AlgoliaSearch\Helper\Data;
+use Algolia\AlgoliaSearch\Model\LandingPage;
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\Registry;
+
+class SearchConfiguration extends \Magento\Backend\Block\Template
+{
+    /** @var string */
+    protected $_template = 'landingpage/search-configuration.phtml';
+
+    /** @var Registry */
+    protected $registry;
+
+    /** @var ConfigHelper */
+    private $configHelper;
+
+    /** @var Data */
+    private $coreHelper;
+
+    /**
+     * @param Context $context
+     * @param Registry $registry
+     * @param ConfigHelper $configHelper
+     * @param Data $coreHelper
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        ConfigHelper $configHelper,
+        Data $coreHelper,
+        array $data = []
+    ) {
+        $this->registry = $registry;
+        $this->configHelper = $configHelper;
+        $this->coreHelper = $coreHelper;
+
+        parent::__construct($context, $data);
+    }
+
+    /** @return LandingPage | null */
+    public function getLandingPage()
+    {
+        return $this->registry->registry('algoliasearch_landing_page');
+    }
+
+    /** @return ConfigHelper */
+    public function getConfigHelper()
+    {
+        return $this->configHelper;
+    }
+
+    /** @return Data */
+    public function getCoreHelper()
+    {
+        return $this->coreHelper;
+    }
+}

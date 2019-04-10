a95630eb67f1ed7f46ccb9498715f95e6b7e6b28
Damien Couchez <damien.couchez@gmail.com>
2019-04-10 11:49:37 +0200

    Magento 2.1 compatibility for ui components

---
+++ Block/Adminhtml/LandingPage/CmsNotice.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Block\Adminhtml\LandingPage;
+
+class CmsNotice extends \Magento\Backend\Block\Template
+{
+    /** @var string */
+    protected $_template = 'landingpage/cms-notice.phtml';
+}
---
+++ Block/Adminhtml/Query/BannerNotice.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Block\Adminhtml\Query;
+
+class BannerNotice extends \Magento\Backend\Block\Template
+{
+    /** @var string */
+    protected $_template = 'query/banner-notice.phtml';
+}

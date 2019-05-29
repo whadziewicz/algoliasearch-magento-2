d2713a2562c1eb2db87df03162e3a8f5ce17c17f
Damien Couchez <damien.couchez@gmail.com>
2019-05-29 11:32:33 +0200

    Adding new default facet renderer

---
+++ Block/Navigation/Renderer/DefaultRenderer.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Block\Navigation\Renderer;
+
+use Magento\Catalog\Model\Layer\Filter\FilterInterface;
+use Magento\Framework\View\Element\Template;
+use Magento\LayeredNavigation\Block\Navigation\FilterRendererInterface;
+
+class DefaultRenderer extends Template implements FilterRendererInterface
+{
+    const JS_COMPONENT = 'Algolia_AlgoliaSearch/navigation/attribute-filter';
+
+    /**
+     * Path to template file.
+     *
+     * @var string
+     */
+    protected $_template = 'Algolia_AlgoliaSearch::layer/filter/js-default.phtml';
+//    protected $_template = 'Algolia_AlgoliaSearch::layer/filter/default.phtml';
+
+    /**
+     * Returns true if checkox have to be enabled.
+     *
+     * @return boolean
+     */
+    public function isMultipleSelectEnabled()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function render(FilterInterface $filter)
+    {
+        $html = '';
+        $this->filter = $filter;
+
+        if ($this->canRenderFilter()) {
+            $this->assign('filterItems', $filter->getItems());
+            $html = $this->_toHtml();
+            $this->assign('filterItems', []);
+        }
+
+        return $html;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getJsLayout()
+    {
+        $filterItems = $this->getFilter()->getItems();
+
+        $jsLayoutConfig = [
+            'component' => self::JS_COMPONENT,
+            'maxSize'  => (int) $this->getFilter()->getAttributeModel()->getFacetMaxSize(),
+            'displayProductCount' => (bool) $this->displayProductCount(),
+            'hasMoreItems' => (bool) $this->getFilter()->hasMoreItems(),
+            'ajaxLoadUrl' => $this->getAjaxLoadUrl(),
+        ];
+
+        foreach ($filterItems as $item) {
+//            $jsLayoutConfig['items'][] = $item->toArray(['label', 'count', 'url', 'is_selected']);
+            $jsLayoutConfig['items'][] = [
+                'label' => $item->getLabel(),
+                'count' => $item->getCount(),
+                'url' => $this->escapeUrl($item->getUrl()),
+                'is_selected' => $item->getData('is_selected')
+            ];
+        }
+
+        return json_encode($jsLayoutConfig);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function canRenderFilter()
+    {
+        return true;
+    }
+
+    /**
+     * @return FilterInterface
+     */
+    public function getFilter()
+    {
+        return $this->filter;
+    }
+
+    /**
+     * Indicates if the product count should be displayed or not.
+     *
+     * @return boolean
+     */
+    public function displayProductCount()
+    {
+        return true;
+//        return $this->catalogHelper->shouldDisplayProductCountOnLayer();
+    }
+
+    /**
+     * Get the AJAX load URL (used by the show more and the search features).
+     *
+     * @return string
+     */
+    private function getAjaxLoadUrl()
+    {
+        $qsParams = ['filterName' => $this->getFilter()->getRequestVar()];
+
+        $currentCategory = $this->getFilter()->getLayer()->getCurrentCategory();
+
+        if ($currentCategory && $currentCategory->getId() && $currentCategory->getLevel() > 1) {
+            $qsParams['cat'] = $currentCategory->getId();
+        }
+
+        $urlParams = ['_current' => true, '_query' => $qsParams];
+
+        return $this->_urlBuilder->getUrl('catalog/navigation_filter/ajax', $urlParams);
+    }
+}
---
+++ Plugin/BackendFilterRenderer.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Plugin;
+
+use Algolia\AlgoliaSearch\Helper\ConfigHelper;
+use Magento\Framework\View\LayoutInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class BackendFilterRenderer
+{
+    /** @var LayoutInterface */
+    protected $layout;
+
+    /** @var string */
+    protected $block = \Algolia\AlgoliaSearch\Block\Navigation\Renderer\DefaultRenderer::class;
+
+    /** @var ConfigHelper */
+    private $configHelper;
+
+    /**
+     * @param LayoutInterface $layout
+     * @param StoreManagerInterface $storeManager
+     * @param ConfigHelper $configHelper
+     */
+    public function __construct(
+        LayoutInterface $layout,
+        StoreManagerInterface $storeManager,
+        ConfigHelper $configHelper
+    ) {
+        $this->layout = $layout;
+        $this->configHelper = $configHelper;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @param \Magento\LayeredNavigation\Block\Navigation\FilterRenderer $subject
+     * @param \Closure $proceed
+     * @param \Magento\Catalog\Model\Layer\Filter\FilterInterface $filter
+     * @return mixed
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function aroundRender(
+        \Magento\LayeredNavigation\Block\Navigation\FilterRenderer $subject,
+        \Closure $proceed,
+        \Magento\Catalog\Model\Layer\Filter\FilterInterface $filter
+    ) {
+        $attributeCode = $filter->getAttributeModel()->getAttributeCode();
+
+        if ($this->configHelper->isBackendRenderingEnabled()) {
+            $facets = $this->configHelper->getFacets($this->storeManager->getStore()->getId());
+
+            foreach ($facets as $facet) {
+                if ($facet['attribute'] == $attributeCode) {
+                    return $this->layout
+                        ->createBlock($this->block)
+                        ->render($filter);
+                }
+            }
+        }
+
+        return $proceed($filter);
+    }
+}

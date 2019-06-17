ad73c5a2641127c46fb5138318a18ca281a5c32c
Jan Petr <jan.petr@algolia.com>
2019-06-17 10:42:03 +0200

    Remove usage of around methods in ProductObserver (#783)

--- Model/Indexer/CategoryObserver.php
+++ Model/Indexer/CategoryObserver.php
@@ -2,44 +2,74 @@
 
 namespace Algolia\AlgoliaSearch\Model\Indexer;
 
+use Algolia\AlgoliaSearch\Helper\ConfigHelper;
 use Algolia\AlgoliaSearch\Model\Indexer\Category as CategoryIndexer;
 use Magento\Catalog\Model\Category as CategoryModel;
 use Magento\Catalog\Model\ResourceModel\Category as CategoryResourceModel;
+use Magento\Catalog\Model\ResourceModel\Product\Collection as ProductCollection;
 use Magento\Framework\Indexer\IndexerRegistry;
 
 class CategoryObserver
 {
+    /** @var CategoryIndexer */
     private $indexer;
 
-    public function __construct(IndexerRegistry $indexerRegistry)
+    /** @var ConfigHelper */
+    private $configHelper;
+
+    /**
+     * @param IndexerRegistry $indexerRegistry
+     * @param ConfigHelper $configHelper
+     */
+    public function __construct(IndexerRegistry $indexerRegistry, ConfigHelper $configHelper)
     {
         $this->indexer = $indexerRegistry->get('algolia_categories');
+        $this->configHelper = $configHelper;
     }
 
-    public function afterSave(
-        CategoryResourceModel $categoryResource,
-        $result,
-        CategoryModel $category
-    ) {
-        if (!$this->indexer->isScheduled()) {
-            /** @var Magento\Catalog\Model\ResourceModel\Product\Collection $productCollection */
-            $productCollection = $category->getProductCollection();
-            CategoryIndexer::$affectedProductIds = (array) $productCollection->getColumnValues('entity_id');
+    /**
+     * Using "before" method here instead of "after", because M2.1 doesn't pass "$product" argument
+     * to "after" methods. When M2.1 support will be removed, this method can be rewriten to:
+     * afterSave(CategoryResourceModel $categoryResource, CategoryResourceModel $result, CategoryModel $category)
+     *
+     * @param CategoryResourceModel $categoryResource
+     * @param CategoryModel $category
+     *
+     * @return CategoryModel[]
+     */
+    public function beforeSave(CategoryResourceModel $categoryResource, CategoryModel $category)
+    {
+        $categoryResource->addCommitCallback(function() use ($category) {
+            if (!$this->indexer->isScheduled() || $this->configHelper->isQueueActive()) {
+                /** @var ProductCollection $productCollection */
+                $productCollection = $category->getProductCollection();
+                CategoryIndexer::$affectedProductIds = (array) $productCollection->getColumnValues('entity_id');
 
-            $this->indexer->reindexRow($category->getId());
-        }
+                $this->indexer->reindexRow($category->getId());
+            }
+        });
+
+        return [$category];
     }
 
-    public function beforeDelete(
-        CategoryResourceModel $categoryResource,
-        CategoryModel $category
-    ) {
-        if (!$this->indexer->isScheduled()) {
-            /* we are using products position because getProductCollection() does use correct store */
-            $productCollection = $category->getProductsPosition();
-            CategoryIndexer::$affectedProductIds = array_keys($productCollection);
+    /**
+     * @param CategoryResourceModel $categoryResource
+     * @param CategoryModel $category
+     *
+     * @return CategoryModel[]
+     */
+    public function beforeDelete(CategoryResourceModel $categoryResource, CategoryModel $category)
+    {
+        $categoryResource->addCommitCallback(function() use ($category) {
+            if (!$this->indexer->isScheduled() || $this->configHelper->isQueueActive()) {
+                /* we are using products position because getProductCollection() doesn't use correct store */
+                $productCollection = $category->getProductsPosition();
+                CategoryIndexer::$affectedProductIds = array_keys($productCollection);
 
-            $this->indexer->reindexRow($category->getId());
-        }
+                $this->indexer->reindexRow($category->getId());
+            }
+        });
+
+        return [$category];
     }
 }

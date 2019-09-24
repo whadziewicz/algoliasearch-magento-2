5ae0a8406472e90b3c4821366cc26a51ec34730e
Betty Suravech <bsuravech@gmail.com>
2019-09-24 15:10:03 -0700

    update setup to split indexing

--- dev/cloud/setup.sh
+++ dev/cloud/setup.sh
@@ -1,9 +1,18 @@
 #! /usr/bin/env bash
 
+php echo 'Saving APPID...'
 php bin/magento config:set algoliasearch_credentials/credentials/application_id "${ALGOLIA_APPLICATION_ID}"
+php echo 'Saving Search Key...'
 php bin/magento config:set algoliasearch_credentials/credentials/search_only_api_key "${ALGOLIA_SEARCH_API_KEY}"
+php echo 'Saving API key...'
 php bin/magento config:set algoliasearch_credentials/credentials/api_key "${ALGOLIA_API_KEY}"
+php echo 'Saving prefix...'
 php bin/magento config:set algoliasearch_credentials/credentials/index_prefix "${MAGENTO_CLOUD_ENVIRONMENT}_"
+php echo 'Saving enable instant...'
 php bin/magento config:set algoliasearch_instant/instant/is_instant_enabled "1"
 
-php bin/magento indexer:reindex algolia_products algolia_categories algolia_pages algolia_suggestions algolia_additional_sections
+php bin/magento indexer:reindex algolia_products
+php bin/magento indexer:reindex algolia_categories
+php bin/magento indexer:reindex algolia_pages
+php bin/magento indexer:reindex algolia_suggestions
+php bin/magento indexer:reindex algolia_additional_sections

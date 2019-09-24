62185a72d289ed3cfdfbdd38a90240ef0e0c713a
Betty Suravech <bsuravech@gmail.com>
2019-09-24 15:26:20 -0700

    update print for setup

--- dev/cloud/setup.sh
+++ dev/cloud/setup.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
-php echo 'Saving APPID...'
+echo 'Saving APPID...'
 php bin/magento config:set algoliasearch_credentials/credentials/application_id "${ALGOLIA_APPLICATION_ID}"
-php echo 'Saving Search Key...'
+echo 'Saving Search Key...'
 php bin/magento config:set algoliasearch_credentials/credentials/search_only_api_key "${ALGOLIA_SEARCH_API_KEY}"
-php echo 'Saving API key...'
+echo 'Saving API key...'
 php bin/magento config:set algoliasearch_credentials/credentials/api_key "${ALGOLIA_API_KEY}"
-php echo 'Saving prefix...'
+echo 'Saving prefix...'
 php bin/magento config:set algoliasearch_credentials/credentials/index_prefix "${MAGENTO_CLOUD_ENVIRONMENT}_"
-php echo 'Saving enable instant...'
+echo 'Saving enable instant...'
 php bin/magento config:set algoliasearch_instant/instant/is_instant_enabled "1"
 
 php bin/magento indexer:reindex algolia_products

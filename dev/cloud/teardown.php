4f925316b890339f42caac6e1532232a6bc3016a
Betty Suravech <bsuravech@gmail.com>
2019-10-01 15:54:10 -0700

    Magento Cloud Development setup teardown (#860)

---
+++ dev/cloud/teardown.php
@@ -0,0 +1,43 @@
+<?php
+
+use Magento\Framework\App\Bootstrap;
+require '/app/app/bootstrap.php';
+
+$bootstrap = Bootstrap::create(BP, $_SERVER);
+$objectManager = $bootstrap->getObjectManager();
+
+$algoliaHelper = $objectManager->get('\Algolia\AlgoliaSearch\Helper\AlgoliaHelper');
+$indexNamePrefix = getenv('MAGENTO_CLOUD_ENVIRONMENT');
+
+/**
+ * @param $algoliaHelper Algolia\AlgoliaSearch\Helper\AlgoliaHelper
+ * @param array $indices
+ */
+function deleteIndexes($algoliaHelper, array $indices, $indexNamePrefix)
+{
+    foreach ($indices['items'] as $index) {
+        $name = $index['name'];
+
+        if (mb_strpos($name, $indexNamePrefix) === 0) {
+            try {
+                $algoliaHelper->deleteIndex($name);
+                echo 'Index "' . $name . '" has been deleted.';
+                echo "\n";
+            } catch (Exception $e) {
+                // Might be a replica
+            }
+        }
+    }
+}
+
+if ($algoliaHelper) {
+    $indices = $algoliaHelper->listIndexes();
+    if (count($indices) > 0) {
+        deleteIndexes($algoliaHelper, $indices, $indexNamePrefix);
+    }
+
+    $replicas = $algoliaHelper->listIndexes();
+    if (count($replicas) > 0) {
+        deleteIndexes($algoliaHelper, $replicas, $indexNamePrefix);
+    }
+}

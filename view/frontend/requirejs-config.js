e6686d1654f26b791f95109b704a8f75b90eeb07
Betty Suravech <bsuravech@gmail.com>
2019-10-14 12:33:21 -0700

    convert parameters from quote item to sales order item

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,5 +3,12 @@ var config = {
 		'algoliaBundle': 'Algolia_AlgoliaSearch/internals/algoliaBundle.min',
 		'algoliaAnalytics': 'Algolia_AlgoliaSearch/internals/search-insights',
 		'rangeSlider': 'Algolia_AlgoliaSearch/navigation/range-slider-widget'
+	},
+	'config': {
+		'mixins': {
+			"Magento_Catalog/js/catalog-add-to-cart": {
+				"Algolia_AlgoliaSearch/js/catalog-add-to-cart-mixin": true
+			}
+		}
 	}
 };

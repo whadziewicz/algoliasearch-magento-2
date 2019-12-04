34135274ce788a536a4dbfc787818d3a4c72aabe
Vladimir Malik <vladimir@convert.no>
2019-12-04 16:19:50 +0300

    Replace class names to ::class instead string names (#936)

--- Model/ResourceModel/Job/Collection.php
+++ Model/ResourceModel/Job/Collection.php
@@ -18,8 +18,8 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
     protected function _construct()
     {
         $this->_init(
-            'Algolia\AlgoliaSearch\Model\Job',
-            'Algolia\AlgoliaSearch\Model\ResourceModel\Job'
+            \Algolia\AlgoliaSearch\Model\Job::class,
+            \Algolia\AlgoliaSearch\Model\ResourceModel\Job::class
         );
     }
 }
